ce3d17765ae0a5d6545412f7844570f687c1633c
Alex <alexcarrascosalvador@gmail.com>
2021-09-07 14:09:53 +0200

    Primer commit app sin effects

---
+++ src/app/usuarios/lista/lista.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../../models/usuario.model';
+import { UsuarioService } from '../../services/usuario.service';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styles: [],
+})
+export class ListaComponent implements OnInit {
+  usuarios: Usuario[] = [];
+  constructor(private usuarioService: UsuarioService) {}
+
+  ngOnInit() {
+    this.usuarioService
+      .getUsers()
+      .subscribe((users) => (this.usuarios = users));
+  }
+}
